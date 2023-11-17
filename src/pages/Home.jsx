dbc9c8a2e645e0a0fad6f2d22084a441838170ad
afifahnn <141509819+afifahnn@users.noreply.github.com>
2023-11-17 16:36:36 +0700

    chapter7

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div className='bg-red-500'>
-        Afifah Nur Nabila cantik
+    <div className='flex justify-center items-center h-screen bg-gray-800'>
+        <div className='text-[3rem] font-extrabold text-center bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-fuchsia-300 via-green-400 to-rose-700 text-transparent bg-clip-text'>
+          Hello :)
+          <br/>Afifah Nur Nabila
+        </div>
     </div>
   )
 }
